862a431d7675fcd73272625c6c1d7f15058697f7
CreizyLabs <creizylabs@gmail.com>
2026-09-25 12:05:58 -0400

    chore: remove all unverified and incomplete formalizations from BountySolves

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,7 @@ open Lake DSL
 
 package "bounty_solves" where
   leanOptions := #[⟨`autoImplicit, false⟩]
+  moreLeanArgs := #["-M", "4096", "-j", "2"]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
@@ -11,120 +12,11 @@ require mathlib from git
 lean_lib "BountySolves" where
   srcDir := "BountySolves"
 
-lean_lib "KaplanskyRefutation" where
-  srcDir := "BountySolves"
-
-lean_lib "DGGCostPreserving" where
-  srcDir := "BountySolves"
-
-lean_lib "LowerBoundMaxElement" where
-  srcDir := "BountySolves"
-
-lean_lib "ThomCobordism" where
-  srcDir := "BountySolves"
-
-lean_lib "KirbyTaylorPin" where
-  srcDir := "BountySolves"
-
-lean_lib "E8Roots" where
-  srcDir := "BountySolves"
-
-lean_lib "ZPhiRing" where
-  srcDir := "BountySolves"
-
-lean_lib "E8Obstruction" where
-  srcDir := "BountySolves"
-
-lean_lib "VacuumDecoupling" where
-  srcDir := "BountySolves"
-
-lean_lib "ErdosAnning" where
-  srcDir := "BountySolves"
-
-lean_lib "SunflowerLemma" where
-  srcDir := "BountySolves"
-
-lean_lib "ScottDomainCPO" where
-  srcDir := "BountySolves"
-
-lean_lib "BeltramiAdvection" where
-  srcDir := "BountySolves"
-
-lean_lib "GolombPowerful" where
-  srcDir := "BountySolves"
-
 lean_lib "StablyCompleteGoldenRatio" where
   srcDir := "BountySolves"
 
 lean_lib "ErdosSimonovitsCompactness" where
   srcDir := "BountySolves"
 
-lean_lib "GoldbachSingular" where
+lean_lib "ErdosGalvinSubsetSums" where
   srcDir := "BountySolves"
-
-lean_lib "BogomolnyBound" where
-  srcDir := "BountySolves"
-
-lean_lib "RegulatorPositivity" where
-  srcDir := "BountySolves"
-
-lean_lib "IdempotentChern" where
-  srcDir := "BountySolves"
-
-lean_lib "HurwitzGoldenRatio" where
-  srcDir := "BountySolves"
-
-lean_lib "FibonacciBraidUnitary" where
-  srcDir := "BountySolves"
-
-lean_lib "IcosianE8Roots" where
-  srcDir := "BountySolves"
-
-lean_lib "GinspargWilsonAnomaly" where
-  srcDir := "BountySolves"
-
-lean_lib "KolmogorovTurbulenceDamping" where
-  srcDir := "BountySolves"
-
-lean_lib "KlesserReversibleSO4" where
-  srcDir := "BountySolves"
-
-lean_lib "HeckeRamanujanLanglands" where
-  srcDir := "BountySolves"
-
-lean_lib "AndersonLocalRings" where
-  srcDir := "BountySolves"
-
-lean_lib "GuysD19" where
-  srcDir := "BountySolves"
-
-lean_lib "OddCoveringSystems" where
-  srcDir := "BountySolves"
-
-lean_lib "ErdosStraus" where
-  srcDir := "BountySolves"
-
-lean_lib "CatalanMihailescu" where
-  srcDir := "BountySolves"
-
-lean_lib "PoincareSphere" where
-  srcDir := "BountySolves"
-
-lean_lib "FranklConjecture" where
-  srcDir := "BountySolves"
-
-lean_lib "ErdosSidonSets" where
-  srcDir := "BountySolves"
-
-lean_lib "AdditiveComplementSquares" where
-  srcDir := "BountySolves"
-
-
-
-
-
-
-
-
-
-
